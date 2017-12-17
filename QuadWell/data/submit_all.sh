fe95a099463708ec960807109b4f327d0ad205b1
Robert Arbon <ra15808@bc4login3.bc4.acrc.priv>
2017-12-17 14:31:01 +0000

    Changed submit scripts

--- QuadWell/data/submit_all.sh
+++ QuadWell/data/submit_all.sh
@@ -4,7 +4,7 @@
 for f in 000.5pc  001.9pc  007.1pc  026.6pc  100.0pc 
 do
    cd $f
-   name=config
-   sbatch --export=config=$name,num=100 --job-name=$f submit.sh
+   name=config-shuffle
+   sbatch --export=config=$name  --job-name=$f --time=06:00:00 submit.sh
    cd ../
 done
